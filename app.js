29deed250173bb432761a7481c3ef9a52437c251
GustavoLon <gustavomrodrigues11@gmail.com>
2024-10-31 11:24:49 -0300

    fix: add public dir

--- app.js
+++ app.js
@@ -2,15 +2,14 @@
 import express from "express";
 // API DO back4Apps
 import parse from "parse/node.js";
-
 // .env
 import env from "dotenv";
 
-env.params = {
-  env: process.env.API_ID,
-  password: process.env.JS_KEY,
-};
-exports = env.params;
+// env.params = {
+//   env: process.env.API_ID,
+//   password: process.env.JS_KEY,
+// };
+// exports = env.params;
 
 const app = express();
 const port = 3000;
@@ -25,6 +24,6 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-initialize(params.API_ID, params.JS_KEY);
-//PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
-serverURL = "https://parseapi.back4app.com/";
+// initialize(params.API_ID, params.JS_KEY);
+// //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+// serverURL = "https://parseapi.back4app.com/";
