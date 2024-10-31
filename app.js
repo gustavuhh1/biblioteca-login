4c96f453a1b5adc8fefbb7653d7dddc0bab3cbce
GustavoLon <gustavomrodrigues11@gmail.com>
2024-10-31 10:50:53 -0300

    fix: resolver isso em casa

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-// API DO back4Apps
-import { initialize, serverURL } from "parse/node";
 // EXPRESS
 import express from "express";
+// API DO back4Apps
+import parse from "parse/node.js";
+
 // .env
-require("dotenv").load();
-params = {
+import env from "dotenv";
+
+env.params = {
   env: process.env.API_ID,
   password: process.env.JS_KEY,
 };
-module.exports = params;
+exports = env.params;
 
 const app = express();
 const port = 3000;
+
+app.use("/static", express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -21,6 +25,6 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-initialize(params.API_ID, params.JS_KEY); 
+initialize(params.API_ID, params.JS_KEY);
 //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
 serverURL = "https://parseapi.back4app.com/";
