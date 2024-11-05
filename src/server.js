b3fafb106e09719dbf5ddba4c76ed79dda8ebcea
GustavoMartinsR <gustavomrodrigues11@gmail.com>
2024-11-05 17:00:30 -0300

    feat:finalizando back-end

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
-import fastify from "fastify";
+// fastify
+import Fastify from "fastify" ;
+import fastifyStatic from "@fastify/static"; "@fastify/static";
+// env
 import { env } from "./env.js";
+// Routes
 import { userRoutes } from "./routes/user.js";
-import fastifyStatic from "fastify-static";
-export const app = fastify()
+// tools
+import { fileURLToPath } from 'url';
+import path from 'path';
 
+export const app = Fastify({logger: true})
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 app.addHook("preHandler", async (request, reply) => {
@@ -12,8 +20,17 @@ app.addHook("preHandler", async (request, reply) => {
 });
 app.register(userRoutes, {
     prefix: "user",
-
 })
+
+app.register(fastifyStatic, {
+    root: path.join(__dirname, '../public/'), // Define a pasta 'public' como a raiz para arquivos estáticos
+    prefix: '/', // Define o prefixo da URL para acessar arquivos (use '/' para a raiz)
+});
+
+app.get('/', (request, reply) => {
+    return reply.sendFile('index.html'); // Serve o arquivo 'index.html' na raiz
+});
+
 
 app.listen({
     host: "RENDER" in process.env ? "0.0.0.0" : "localhost", 
